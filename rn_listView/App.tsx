2da2ecdebdfcee49da47a66a31aaf248d8208e85
duycuongit <118497439+duycuongit@users.noreply.github.com>
2023-04-02 20:44:35 +0700

    learn list view

---
+++ rn_listView/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react'
+import List from './List.js'
+
+const App = () => {
+   return (
+      <List />
+   )
+}
+export default App
